1e358f9482a7ffb10c76860ed05086f9441d47eb
samirtf <samircc20092@gmail.com>
2018-03-08 10:53:56 -0300

    fixing socket server

--- tcp-spring-boot-starter/src/main/java/br/com/cantinho/tcpspringbootstarter/ApplicationConfig.java
+++ tcp-spring-boot-starter/src/main/java/br/com/cantinho/tcpspringbootstarter/ApplicationConfig.java
@@ -5,5 +5,5 @@ public final class ApplicationConfig {
   public static final String SECURE_SERVER_IMPLEMENTATION = "SecureTcpThreadPoolServer";
   public static final String NON_SECURE_SERVER_IMPLEMENTATION = "TcpThreadPoolServer";
 
-  public static final String CURRENT_SERVER_IMPLEMENTATION = NON_SECURE_SERVER_IMPLEMENTATION;
+  public static final String CURRENT_SERVER_IMPLEMENTATION = SECURE_SERVER_IMPLEMENTATION;
 }
